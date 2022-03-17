8b8a411f4ce7947ebb8a9100dac9c648896bb685
Sandile Buthelezi <99509906+Shenge9@users.noreply.github.com>
2022-03-17 16:33:37 +0200

    c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,21 @@
 include "main.h"
 
-
 /**
 * more_numbers - prints 10 times the numbers
 */
 void more_numbers(void)
 {
-        int i;
-        char c;
+	int i;
+	char c;
 
-        for (i = 0; i < 10; i++)
-        {
-                for (c = 0; c <= 14; c++)
-                {
-                        if (c > 9)
-                        _putchar((c / 10) + '0');
-                        _putchar((c % 10) + '0');
-                }
-                _putchar('\n');
-        }
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
