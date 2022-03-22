e6d7facfe1f013451106594b186ee9bef71f8a1f
Sandile Buthelezi <99509906+Shenge9@users.noreply.github.com>
2022-03-22 15:35:38 +0200

    c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: Our string Value
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int l = 0;
+	char *y = s;
+	int e = 0;
+	int j;
+	char i;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for ( ; e < ((l / 2) + 1) ; e++)
+	{
+		j = (l - e);
+		i = s[e];
+		s[e] = s[j];
+		s[j] = i;
+	}
+}
