620c9702223e90a1ad96be15db5e6642738347f5
Sandile Buthelezi <99509906+Shenge9@users.noreply.github.com>
2022-03-22 15:48:38 +0200

    c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,11 @@ void print_array(int *a, int n)
 
 	for (i = 0 ; i < n; i++)
 	{
-		printf("%d", a[i]);
+		putchar("%d", a[i]);
 		if (i != n - 1)
 		{
-			printf(", ");
+			putchar(", ");
 		}
 	}
-	printf("\n");
+	putchar("\n");
 }
