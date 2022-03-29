f4ecc3a2eb93c460638fc6fb4a480cf205bf5086
Sandile Buthelezi <99509906+Shenge9@users.noreply.github.com>
2022-03-29 14:02:55 +0200

    c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,22 @@
 #include "main.h"
+#include <stddef.h>
 /**
-* set_string - Sets the value of a pointer to a char
-* @s:  points to the adderss of string
-* @to: string value
-*
-* Return: nothing
+ * _strchr - ocates a character in a string
+ * @s: string value
+ * @c: character value
+ *
+ * Return: 0 (always)
  */
-void set_string(char **s, char *to)
+char *_strchr(char *s, char c)
 {
-	*s = to;
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (0);
 }
