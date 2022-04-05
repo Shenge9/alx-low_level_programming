04e1ac69c41364d06b5dfa0e48d532202eab2c81
sandile <buthelezis28@gmail.com>
2022-04-05 06:35:42 -0700

    c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char.
+ * @size: size of an array
+ * @c: char value
+ * Return: pointer to array / null
+ **/
+char *create_array(unsigned int size, char c)
+{
+	char *string;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	string = malloc(sizeof(char) * size);
+	if (string == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		string[i] = c;
+		i++;
+	}
+	return (string);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+
+The malloc function will request a block of memory from the heap. If the request is granted, the operating system will reserve the requested amount of memory. When the amount of memory is not needed anymore, you must return it to the operating system by calling the function free.
